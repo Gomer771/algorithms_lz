9859c56fe733deb83860ecf35144d5d7ee3339f0
gomer <swistak@tutamail.com>
2024-11-11 17:31:05 +0300

    comment1

--- sort.py
+++ sort.py
@@ -2,15 +2,18 @@ import random
 
 def main():
     my_list=[] 
+    counter=0
     for i in range(1,101):
         my_list.append(random.randint(1,1000000))
     print(my_list)
     print('-'*200)
     for i in range(99):
         for j in range(99-i):
+            counter+=1
             if my_list[j] > my_list[j+1]:
                 my_list[j], my_list[j+1] = my_list[j+1], my_list[j] 
                 print(my_list)
+    print(counter)
 
 main()
 
